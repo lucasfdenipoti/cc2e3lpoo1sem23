296f7d05f060da037d18e16405fbb450655382ee
Lucas Denipoti <lucasfdenipoti@gmail.com>
2023-03-22 15:06:48 -0300

    Controle commit

--- CalculadoraMetodos/src/modelo/Controle.java
+++ CalculadoraMetodos/src/modelo/Controle.java
@@ -4,6 +4,17 @@ public class Controle
 {
     public String executar(String numero1, String numero2, String operacao)
     {
+        Validacao validacao = new Validacao();
+        validacao.validar(numero1, numero2, operacao);
         
+        if(validacao.mensagem.equals(""))
+        {
+            Calculos calculos = new Calculos();
+            calculos.calcular(validacao.num1, validacao.num2, operacao);
+        }
+        else
+        {
+            
+        }
     }
 }
